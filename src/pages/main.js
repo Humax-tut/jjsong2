a4335f9b575246c094a848ec8b0010ca7b063837
JeongjuSong <pc1541@naver.com>
2021-05-24 17:57:35 +0900

    210524. Add Redux-Persist

--- src/pages/main.js
+++ src/pages/main.js
@@ -5,13 +5,13 @@ import { useHistory } from "react-router-dom";
 function Main() {
   const history = useHistory();
   const dispatch = useDispatch();
-  const getUser = localStorage.getItem("persist:root");
 // 다시 Object로 변환
   const myObject = JSON.parse(localStorage.getItem("persist:root")).user;
   const userInf = JSON.parse(myObject);
+  console.log(myObject);
 
-  const empname = userInf.name;
-  const email = userInf.email;
+  // const empname = userInf.name;
+  // const email = userInf.email;
 
   const Logout = () => {
     dispatch(
@@ -25,8 +25,8 @@ function Main() {
       <h1>Main 화면</h1>
 
       <button onClick={Logout}>로그아웃</button>
-      <p>이름 : {empname}</p>
-      <p>이메일 : {email}</p>
+      {/* <p>이름 : {empname}</p>
+      <p>이메일 : {email}</p> */}
     </div>
   );
 }

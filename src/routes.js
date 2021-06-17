07fbbd0be833cb61a499e4199d1eedc4511c0ece
JeongjuSong <pc1541@naver.com>
2021-06-17 15:50:50 +0900

    210617. 404, router

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,19 @@ import Register from './pages/register';
 import Main from './pages/main';
 import Usermng from './pages/usermng';
 import Usermng_View from './pages/usermng_view';
-import Test from './pages/test';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NotFound from './pages/404';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default () => (
     <Router>
-      <Route path="/register" component={Register} />
-      <Route path="/main" component={Main} />
-      <Route path="/login" component={Login} />
-      <Route path="/usermng" component={Usermng} />
-      <Route path="/usermng_view" component={Usermng_View} />
-      <Route path="/test" component={Test} />
-      <Route exact path="/" component={Login} />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/main" component={Main} />
+        <Route path="/login" component={Login} />
+        <Route path="/usermng" component={Usermng} />
+        <Route path="/usermng_view" component={Usermng_View} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   )

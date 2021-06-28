981a80e00a548eec4122512cd2fd0fe5d8dbebe3
yuna <kya7637@hanmail.net>
2021-06-28 17:38:33 +0900

    Update routes.js
    .

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,33 @@
+import React from 'react';
 import Login from './pages/login';
 import Register from './pages/register';
 import Main from './pages/main';
 import Usermng from './pages/usermng';
 import Usermng_View from './pages/usermng_view';
 import NotFound from './pages/404';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default () => (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/main" component={Main} />
-        <Route path="/login" component={Login} />
-        <Route path="/usermng" component={Usermng} />
-        <Route path="/usermng_view" component={Usermng_View} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  )
+class App extends React.Component {
+  render() {
+    return (
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/main" component={Main} />
+          <Route path="/login" component={Login} />
+          <Route path="/usermng" component={Usermng} />
+          <Route path="/usermng_view" component={Usermng_View} />
+          <Route component={NotFound} />
+        </Switch>
+    );
+  }
+}
+
+
+const mapStateToProps = ({ settings, auth }) => {
+  return {
+  };
+};
+
+export default withRouter(connect(mapStateToProps, {  })(App));;

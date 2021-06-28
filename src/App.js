de2801395d1fcdb40f09c896e2d89784dde05349
yuna <kya7637@hanmail.net>
2021-06-28 17:38:16 +0900

    Update App.js
    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import Routes from './routes';
+import React, { Component } from 'react';
+import MainApp from './routes';
+import { Redirect, Route, Switch, BrowserRouter } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
 
-// AutoCheck 로컬스토리지 불러오기 
 
-const App = () => {
+// authUser 정보를 확인하여 정보가 없으면 login 페이지로 이동~
+const RestrictedRoute = ({ component: Component, callback: Callback, authUser, ...rest }) => {
   return (
-    <div>
-      <Routes />
-    </div>
-  )
+    <Route
+      {...rest}
+      render={props => {
+        if (authUser) {
+          return <Component />;
+        }
+        
+        return <Callback />;
+      }}
+    />
+  );
+};
+
+const GoToMainPage = () => {
+  alert("로그인을 해야 접근 가능합니다.");
+  return <Redirect to="/login" />;
+};
+
+
+class App extends Component {
+  render() {
+    
+    // AutoCheck 로컬스토리지 불러오기
+    const myObject = JSON.parse(localStorage.getItem("persist:root")).user;
+    const authenticated = myObject !== null && myObject !== undefined && myObject !== "null" && myObject !== "undefined";
+    
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <RestrictedRoute callback={GoToMainPage} component={MainApp} authUser={authenticated} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;

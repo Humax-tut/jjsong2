3d48e57812edb39f42e866eeb8aa94d32e43482a
yuna <kya7637@hanmail.net>
2021-07-15 17:42:38 +0900

    .

--- src/pages/board.js
+++ src/pages/board.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles, useTheme } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Select from 'react-select';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Paper from "@material-ui/core/Paper";
 import { getUserAll } from "./service";
 
+// 커스텀 스타일
 const useStyles = {
   table: {
     minWidth: 650,
     maxWidth: 1200,
     margin: "auto",
   },
+  title: {
+    minWidth: 650,
+    maxWidth: 1200,
+    margin: "auto",
+    paddingBottom: '30px',
+  },
+  searchArea: {
+    minWidth: 650,
+    maxWidth: 1200,
+    margin: "auto",
+    border: '1px solid gray',
+    padding: '10px',
+  },
+  selectBox: {
+    width: '300px',
+    marginRight: '10px',
+  }
 };
 
 const Activeoptions = [
@@ -24,23 +43,34 @@ const Activeoptions = [
   { value: '0', label: '비활성화' },
 ];
 
+
 class BasicTable extends Component {
-  getUserData = async () => {
-    const rowss = await getUserAll(this.state);
-    this.setState({ rows: rowss });
+  
+  state = {
+    selectedOption: '',
+    searchText: '',
+    isChanged: false,
+    rows: [],
   };
 
+  // lifecycle, 
   componentDidMount() {
     this.setState({ isChanged: true });
   }
 
-  // 회원정보 불러온 뒤 렌더링
+  // lifecycle, 회원정보 불러온 뒤 렌더링
   componentDidUpdate() {
     if (this.state.isChanged) {
       this.getUserData();
       this.setState({ isChanged: false });
     }
   }
+
+  // 메소드
+  getUserData = async () => {
+    const rowss = await getUserAll(this.state);
+    this.setState({ rows: rowss });
+  };
 
   goToMain() {
     window.location.replace("/main");
@@ -49,16 +79,12 @@ class BasicTable extends Component {
   goToDetail(a) {
     window.open('/usermng_view?id=' + a, 'User_Mng', "width=900,height=600");
   }
+  
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ selectedOption: event.target.value, isChanged: true });
+  };
 
-  state = {
-    selectedOption: null,
-    searchText: '',
-    isChanged: false,
-    rows: [],
-  };
-  handleChange = selectedOption => {
-    this.setState({ selectedOption: selectedOption, isChanged: true });
-  };
   onChangeSearchText = searchText => {
     this.setState({ searchText: searchText.target.value, isChanged: true });
   }
@@ -66,27 +92,24 @@ class BasicTable extends Component {
   render() {
     const { selectedOption } = this.state;
     const { classes } = this.props;
+
     return (
       <div>
-        <div>
-          <h2>회원 관리</h2>
+        <div className={classes.title}>
+          <h2>게시판</h2>
           <button onClick={this.goToMain}>메인화면 이동</button>
         </div>
         <div>
-          <div style={{width : '20em'}}>
-          <Select
-            value={selectedOption}
-            onChange={this.handleChange}
-            options={Activeoptions}
-          />
+          <div className={classes.searchArea}>
+            <Select className={classes.selectBox} value={selectedOption} onChange={this.handleChange} >
+              {Activeoptions.map((option) => (
+                  <MenuItem key={option.value} value={option.value} >
+                    {option.label}
+                  </MenuItem>
+                ))}
+            </Select>
+            <input type='text' id='txtSearch' placeholder='이름, 이메일, UserID...' onChange={this.onChangeSearchText}></input>
           </div>
-          <select id='_role'>
-            <option value=''>전체</option>
-            <option value='GENERAL'>GENERAL</option>
-            <option value='Role.Legal.SuperAdmin'>SUPER ADMIN</option>
-            <option value='Role.Legal.InternalLawyer'>INTERNAL LAWYER</option>
-          </select>
-          <input type='text' id='txtSearch' placeholder='이름, 이메일, UserID...' onChange={this.onChangeSearchText}></input>
           <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="simple table">
               <TableHead>

44f77e4800c58dbb365cb773642b6e960775c038
yuna <kya7637@hanmail.net>
2021-07-19 13:58:19 +0900

    .

--- src/pages/test2.js
+++ src/pages/test2.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Test2(props) {
   
+  let cnt = 0;
+  const [state, setState] = useState(props.user+'~'+(cnt));
+
   const showMessage = () => {
     alert('Followed ' + props.user);
   };
 
   const handleClick = () => {
+    cnt = cnt + 1;
+    const userInfo = props.user+'_'+(cnt);
+    setState(userInfo);
     setTimeout(showMessage, 3000);
   };
 
+  console.log('function이 새로 랜더링?? ', state);
+
   return (
-    <button onClick={handleClick}>Function Button</button>
+    <button onClick={handleClick}>Function Button.. {state}</button>
   );
 }
 
